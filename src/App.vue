8cc78686e5a29e733dc6ddf330f59fd8e66d6ea5
qw-null <qinweirz@qq.com>
2021-06-18 14:59:34 +0800

    finish

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,132 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <a-input
+      placeholder="请输入任务"
+      class="my_ipt"
+      :value="inputValue"
+      @change="handleInputChange"
+    />
+    <a-button type="primary" @click="addItemToList">添加事项</a-button>
+
+    <a-list bordered :dataSource="infolist" class="dt_list">
+      <a-list-item slot="renderItem" slot-scope="item">
+        <!-- 复选框 -->
+        <a-checkbox
+          :checked="item.done"
+          @change="
+            e => {
+              cbStatusChanged(e, item.id)
+            }
+          "
+          >{{ item.info }}</a-checkbox
+        >
+        <!-- 删除链接 -->
+        <a slot="actions" @click="removeItembyId(item.id)">删除</a>
+      </a-list-item>
+
+      <!-- footer区域 -->
+      <div slot="footer" class="footer">
+        <!-- 未完成的任务个数 -->
+        <span>{{ unDoneLength }}条剩余</span>
+        <!-- 操作按钮 -->
+        <a-button-group>
+          <a-button
+            @click="ChangeList('all')"
+            :type="viewKey === 'all' ? 'primary' : ''"
+            >全部</a-button
+          >
+          <a-button
+            @click="ChangeList('undone')"
+            :type="viewKey === 'undone' ? 'primary' : ''"
+            >未完成</a-button
+          >
+          <a-button
+            @click="ChangeList('done')"
+            :type="viewKey === 'done' ? 'primary' : ''"
+            >已完成</a-button
+          >
+        </a-button-group>
+        <!-- 把已经完成的任务清空 -->
+        <a @click="clean">清除已完成</a>
+      </div>
+    </a-list>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import { mapState, mapGetters } from 'vuex'
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: 'app',
+  created() {
+    this.$store.dispatch('getList')
+  },
+  data() {
+    return {}
+  },
+  computed: {
+    ...mapState(['inputValue', 'viewKey']),
+    ...mapGetters(['unDoneLength', 'infolist'])
+  },
+  methods: {
+    // 监听文本框内容变化
+    handleInputChange(e) {
+      console.log(e.target.value)
+      this.$store.commit('setInputValue', e.target.value)
+    },
+    // 向列表中新增item项
+    addItemToList() {
+      // trim() 方法用于删除字符串的头尾空白符，空白符包括：空格、制表符 tab、换行符等其他空白符等。
+      if (this.inputValue.trim().length <= 0) {
+        return this.$message.warning('文本框内容不能为空！')
+      }
+      this.$store.commit('addItem')
+    },
+    // 根据id删除对应事件
+    removeItembyId(id) {
+      console.log(id)
+      this.$store.commit('removeItem', id)
+    },
+    // 监听复选框选中状态变化的事件
+    cbStatusChanged(e, id) {
+      // 通过e.target.checked可以接受到最新的选中状态
+      // console.log(e.target.checked)
+      const param = {
+        id: id,
+        status: e.target.checked
+      }
+      this.$store.commit('changeStatus', param)
+    },
+    clean() {
+      //清楚已完成的任务
+      this.$store.commit('cleanDone')
+    },
+    ChangeList(key) {
+      //修改页面上展示的列表数据
+      console.log(key)
+      this.$store.commit('changeviewKey', key)
+    }
   }
 }
 </script>
 
-<style>
+<style scoped>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  padding: 10px;
+}
+
+.my_ipt {
+  width: 500px;
+  margin-right: 10px;
+}
+
+.dt_list {
+  width: 500px;
+  margin-top: 10px;
+}
+
+.footer {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 }
 </style>
